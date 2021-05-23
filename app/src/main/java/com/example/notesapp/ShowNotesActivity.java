a454d69d30e35869c7bdd04554c277728759b4cc
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-23 22:26:57 +1000

    ShowNotesActivity Completed

--- app/src/main/java/com/example/notesapp/ShowNotesActivity.java
+++ app/src/main/java/com/example/notesapp/ShowNotesActivity.java
@@ -1,14 +1,32 @@
 package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.notesapp.data.DatabaseHelper;
+
 public class ShowNotesActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_notes);
+
+        //RecyclerView and DB setup
+        recyclerView = findViewById(R.id.notesRecyclerView);
+        db = new DatabaseHelper(this);
+        setRecyclerView();
+    }
+
+    public void setRecyclerView(){
+        recyclerViewAdapter = new RecyclerViewAdapter(db.getAllNotes(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }

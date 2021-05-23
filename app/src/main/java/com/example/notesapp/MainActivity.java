f638ac08f9383020460be4ea0ab62f4a64fcd5de
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-23 14:13:39 +1000

    MainActivity XML Created

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.notesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button newNoteButton, showNotesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newNoteButton = findViewById(R.id.newNoteButton);
+        showNotesButton = findViewById(R.id.showNotesButton);
+
+        newNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        showNotesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
